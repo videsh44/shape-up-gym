1668c263e3e56f60acdeca36739419809cb8bb07
videsh <videsh.ghodarop@iaugmentor.com>
2022-07-03 15:09:01 +0530

    fixed build || one more

--- firebase/config.js
+++ firebase/config.js
@@ -8,7 +8,6 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
-import 'Firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
